3d8f65993610a5a6791fe7375ad36476e38e0dec
yvieira-@c2r3p1.42sp.org.br <yvieira-@c2r3p1.42sp.org.br>
2025-01-23 19:05:30 -0300

    Adding function to show who key was pressed

--- init_win.c
+++ init_win.c
@@ -1,6 +1,13 @@
 #include <mlx.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <X11/X.h>
+
+// Definição da função key_press
+int key_press(int keycode, void *param) {
+    printf("Key pressed: %d\n", keycode);
+    return 0;
+}
 
 int main() {
     void *mlx;
@@ -21,7 +28,12 @@ int main() {
         free(mlx);
         return (1);
     }
+
+    // Configurando o hook para eventos de pressionamento de tecla
+    mlx_hook(win, KeyPress, KeyPressMask, key_press, NULL);
+
     mlx_loop(mlx);
+
     mlx_destroy_window(mlx, win);
     mlx_destroy_display(mlx);
     return (0);
