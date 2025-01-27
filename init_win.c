be6a792a86a7b031be0c05d1719b3fc14a3830de
yvieira-@c2r3p1.42sp.org.br <yvieira-@c2r3p1.42sp.org.br>
2025-01-27 19:14:57 -0300

    Tentando aplicar o zoom no Mandelbrot

--- init_win.c
+++ init_win.c
@@ -1,91 +1,122 @@
 #include <mlx.h>
-#include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 #include <X11/X.h>
+
+#define WIDTH 800
+#define HEIGHT 600
+#define MAX_ITER 100
+
+typedef struct s_data {
+    void    *mlx;
+    void    *win;
+    void    *img;
+    char    *addr;
+    int     bits_per_pixel;
+    int     line_length;
+    int     endian;
+    double  zoom;
+    double  offset_x;
+    double  offset_y;
+    double  x_min;
+    double  x_max;
+    double  y_min;
+    double  y_max;
+}              t_data;
+
+int close_window(void *param)
+{
+    mlx_loop_end(param);
+    return 0;
+}
 
 int key_press(int keycode, void *param)
 {
     if (keycode == 65307) 
     {
         mlx_loop_end(param);
-
         free(param);
     }
-    printf("Key pressed: %d\n", keycode);
     return(0);
 }
-//preciso criar uma funçao para printar imagem
-int close_window(void *param)
-{
-    mlx_loop_end(param); // Encerra o loop do MiniLibX
-    return 0;
+
+int mandelbrot(double cr, double ci, int max_iter) {
+    double zr = 0.0, zi = 0.0;
+    int n = 0;
+
+    while (n < max_iter) {
+        double zr2 = zr * zr;
+        double zi2 = zi * zi;
+
+        if (zr2 + zi2 > 4.0)
+            return n;
+
+        zi = 2 * zr * zi + ci;
+        zr = zr2 - zi2 + cr;
+        n++;
+    }
+
+    return max_iter;
 }
 
-
-int main() {
-    void *mlx;
-    void *win;
-
-    mlx = mlx_init();
-    if (mlx == NULL)
+void draw_mandelbrot(t_data *data) {
+    int py = 0;
+    while (py < HEIGHT) 
     {
-        perror("mlx init Failed");
-        return (1);
+        int px = 0;
+        while (px < WIDTH)
+        {
+            double cr = data->x_min + (data->x_max - data->x_min) * px / WIDTH;
+            double ci = data->y_min + (data->y_max - data->y_min) * py / HEIGHT;
+
+            int color = mandelbrot(cr, ci, MAX_ITER);
+            int pixel_color = (color << 21) | (color << 10) | (color << 3);
+
+            int pixel = py * data->line_length + px * (data->bits_per_pixel / 8);
+            *(int *)(data->addr + pixel) = pixel_color;
+            px++;
+        }
+        py++;
     }
+    mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
+}
 
-    win = mlx_new_window(mlx, 800, 600, "MiniLibX Window");
-    if (win == NULL)
-    {
-        perror("win_new_windows failed");
-        mlx_destroy_display(mlx);
-        free(mlx);
-        return (1);
-    }
-
-    int i = 0 ;
-    int j = 0 ;
-   
-	int	x = 0;
-	int	y = 0;
-    int	x0 = x;
-	int	y0 = y ;
-	int color = 000;
-	int xtemp = 0;
-	int	iteration = 111;
-	int	max_iteration = 1000;
-	while (i < max_iteration)
-	{
-		while ( x*x + y*y <= (2*2) && iteration < max_iteration)
-		{
-			xtemp = x*x - y*y + x0;
-			y = 2*x*y + y0;
-
-			x = xtemp;
-
-			iteration = iteration + 1;
-		}
-
-		if ( iteration == max_iteration )
-		{
-			color = 000;
-			mlx_pixel_put(mlx, win, x0, y0, color);	
-		}
-		else
-			color = iteration;
-
-		mlx_pixel_put(mlx, win, x0, y0, color);
-		j++;
-		i++;
-	}
-    mlx_hook(win, KeyPress, KeyPressMask, key_press, NULL);
-    mlx_hook(win, DestroyNotify, NoEventMask, close_window, mlx);
-    mlx_loop(mlx);
-
-
-
-    mlx_destroy_window(mlx, win);
-    mlx_destroy_display(mlx);
-    free(mlx);
-    free(win);
+int mouse_hook(int button, int x, int y, t_data *data)
+{
+    double mouse_re = data->x_min + (data->x_max - data->x_min) * x / WIDTH;
+    double mouse_im = data->y_min + (data->y_max - data->y_min) * y / HEIGHT;
+    if (button == 4)
+        data->zoom *= 1.1;
+    else if (button == 5)
+        data->zoom /= 1.1;
+    double zoom_factor = 1.0 / data->zoom;
+    data->x_min = mouse_re - (mouse_re - data->x_min) * zoom_factor;
+    data->x_max = mouse_re + (data->x_max - mouse_re) * zoom_factor;
+    data->y_min = mouse_im - (mouse_im - data->y_min) * zoom_factor;
+    data->y_max = mouse_im + (data->y_max - mouse_im) * zoom_factor;
+    draw_mandelbrot(data);
     return (0);
+}
+
+int main(void) {
+    t_data data;
+
+    data.mlx = mlx_init();
+    data.win = mlx_new_window(data.mlx, WIDTH, HEIGHT, "Mandelbrot Set");
+    data.img = mlx_new_image(data.mlx, WIDTH, HEIGHT);
+    data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
+    data.zoom = 1.0;
+    data.x_min = -2.0;
+    data.x_max = 1.0;
+    data.y_min = -1.5;
+    data.y_max = 1.5;
+
+    draw_mandelbrot(&data);
+
+    mlx_hook(data.win, KeyPress, KeyPressMask, key_press, NULL);
+    mlx_mouse_hook(data.win, mouse_hook, &data);
+    mlx_hook(data.win, DestroyNotify, NoEventMask, close_window, data.mlx);
+    mlx_loop(data.mlx);
+
+    return 0;
 }
